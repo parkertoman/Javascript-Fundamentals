453da732fc84d9d3d9a3c1fc421204ef8a021a99
Parker Toman <parkertoman@outlook.com>
2026-04-22 13:20:30 -0500

    Lesson 2 lab

---
+++ lesson2.js
@@ -0,0 +1,16 @@
+const userName = 'John Smith';
+let fruit = 'Mango';
+fruit = 'Grape';
+
+// console.log(userName);
+// console.log(fruit)
+// console.log("Hello world!")
+
+const numberOne = 1;
+const amount = 500;
+
+console.log(numberOne);
+console.log(amount);
+
+const FULL_NAME = "John Doe";
+const AMOUNT = 1000;

5d0ffa52b9f07d6d64ea56c7be4c91417f591799
parkertoman <parkertoman@outlook.com>
2026-04-24 13:20:47 -0500

    Strings lab

---
+++ labs/stringslab.js
@@ -0,0 +1,9 @@
+const string = " This statement has empty spaces ";
+
+console.log(string.includes("pt"));
+console.log(string.at(7))
+console.log(string.trim())
+console.log(string.toLowerCase())
+
+console.log("Jack and Jill went up the hill to fetch a pail of water. " +
+            "Jack fell down and broke his crown and Jill came tumbling after.")

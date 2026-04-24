bff81b4784cefcdf5c13826760087c2da8777e66
parkertoman <parkertoman@outlook.com>
2026-04-24 16:24:29 -0500

    Numbers lab

---
+++ labs/numberslab.js
@@ -0,0 +1,19 @@
+const value1 = 34_038;
+const value2 = 45_838.8734;
+const value3 = 0b0110_0010_1011;
+const value4 = 0x1_74_C0;
+const value5 = -895_612;
+
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
+console.log(value5);
+
+const valueHexadecimal = 0x1D;
+const valueBinary = 0b1_1101;
+const valueOctal = 0o35;
+
+console.log(valueHexadecimal);
+console.log(valueBinary);
+console.log(valueOctal);

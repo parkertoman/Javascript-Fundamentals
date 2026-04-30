5932e8ac91422bd465fe330c823aae213525b9a5
parkertoman <parkertoman@outlook.com>
2026-04-30 00:52:12 -0500

    Arrays lab

---
+++ labs/arrayslab.js
@@ -0,0 +1,6 @@
+const iceCream = ["Chocolate", "Vanilla", "Strawberry", "Neapolitan", "Cookies and Cream", "Chocolate Chip", "Mint Chip", "Brownie", "Sorbet", "Cookie Dough"];
+for(const flavor of iceCream){
+    console.log(flavor);
+};
+const flavorList = iceCream.join(' & ')
+console.log(flavorList);

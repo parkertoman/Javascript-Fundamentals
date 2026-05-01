e62e8c3183a3b998d172a14994c26f03c6c198ba
parkertoman <parkertoman@outlook.com>
2026-05-01 12:21:21 -0500

    Maps lesson

---
+++ lessons/map.js
@@ -0,0 +1,50 @@
+// const programmingLanguages = new Map();
+// programmingLanguages.set("JS", "JavaScript");
+// programmingLanguages.set("PY", "Python");
+// programmingLanguages.set("JA", "Java");
+// programmingLanguages.set("RR", "Ruby on Rails");
+
+const programmingLanguages = new Map([
+    ["JS", "JavaScript"], 
+    ["PY", "Python"], 
+    ["JA", "Java"], 
+    ["RR", "Ruby on Rails"]
+]);
+
+// console.log(programmingLanguages.size);
+// programmingLanguages.clear();
+
+// const result = programmingLanguages.delete("Rr");
+// console.log(result);
+
+// const language = programmingLanguages.get("JS");
+// console.log(language);
+
+// console.log(programmingLanguages.has("Ja"));
+
+// Loop a map collection
+// for (let lang of programmingLanguages){
+//     console.log(lang);
+// }
+
+// for (let key of programmingLanguages.keys()) {
+//     console.log(key);
+// }
+
+// for (let item of programmingLanguages.entries()) {
+//     console.log(item);
+// }
+
+// function processData(value, key){
+//     console.log(`The key is ${key}. The value is ${value}.`);
+// }
+
+// programmingLanguages.forEach(processData);
+
+// programmingLanguages.forEach((value, key) => {
+//     console.log(`The key is ${key}. The value is ${value}.`);
+// })
+
+programmingLanguages.forEach(function processData(value, key) {
+    console.log(`The key is ${key}. The value is ${value}.`);
+})
